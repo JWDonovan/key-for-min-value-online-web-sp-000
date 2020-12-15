a4096e04d015e411c41dc570550bfeb29ce08de4
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 15:18:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,25 +17,3 @@ def key_for_min_value(name_hash)
 
   array.first
 end
-
-def bubble_sort(array)
-  array_length = array.size
-  return array if array_length <= 1
-
-  loop do
-    # we need to create a variable that will be checked so that we don't run into an infinite loop scenario.
-    swapped = false
-
-    # subtract one because Ruby arrays are zero-index based
-    (array_length-1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
-        swapped = true
-      end
-    end
-
-    break if not swapped
-  end
-
-  array
-end
